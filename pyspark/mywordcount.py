a249bbb46caab63d298a1f2060cd50271648e0e7
LLL <1043876586@qq.com>
2021-04-10 20:26:30 +0800

    wordcount编写

--- pyspark/mywordcount.py
+++ pyspark/mywordcount.py
@@ -1,6 +1,6 @@
 from pyspark import SparkContext
 sc = SparkContext( 'local', 'mywordcount')
-textFile = sc.textFile("file:///opt/txt/python/pyspark/data.txt")
+textFile = sc.textFile("file:///opt/text/python/pyspark/data.txt")
 wordCount = textFile.flatMap(lambda line: line.split(" ")).map(lambda word: (word,1)).reduceByKey(lambda a, b : a + b)
 wordCount.collect()
 wordCount.foreach(print)
